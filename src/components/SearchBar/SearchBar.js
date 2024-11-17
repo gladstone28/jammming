7e4d2d0fd7a2049917874d06c23630c521b20d9d
Gladstone Rose <gladstonerose@hotmail.com>
2024-11-16 19:58:43 -0500

    add structure with files

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './SearchBar.css';
+
+const SearchBar = ({ onSearch }) => {
+  const [term, setTerm] = useState('');
+
+  const handleSearch = () => {
+    onSearch(term);
+  };
+
+  const handleTermChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <div className="SearchBar">
+      <input placeholder="Enter A Song, Album, or Artist" onChange={handleTermChange} />
+      <button className="SearchButton" onClick={handleSearch}>SEARCH</button>
+    </div>
+  );
+};
+
+export default SearchBar;
